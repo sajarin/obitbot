735f2b7caea4d81fecc3765a5f73082babec463f
sajarindider <sajarindider@gmail.com>
2021-11-16 17:18:16 -0500

    chore: restructure & cleanup of old files

--- scrape.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def save_page(storage_path: str, url: str) -> None:
-    response = requests.get(url)
-    html = response.text
-    with open(f'{storage_path}', 'w', encoding="utf-8") as f:
-        f.write(html)
